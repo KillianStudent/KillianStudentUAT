f05a836986e26b78183be3227a2e1d3fcb667a88
KillianStudent <54915178+KillianStudent@users.noreply.github.com>
2019-09-25 14:22:04 -0700

    Class commit 9/25

---
+++ tank game/New Unity Project/Assets/Scripts/AIController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(TankMotor))]
+public class AIController : MonoBehaviour
+{
+    public Transform[] waypoints;
+    public float closeEnough = 1.0f;
+
+    private int currentWaypoint = 0;
+    private TankMotor motor;
+    private TankData data;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        motor = gameObject.GetComponent<TankMotor>();
+        data = gameObject.GetComponent<TankData>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (motor.RotateToward(waypoints[currentWaypoint].position, )
+        {
+            // Nothing
+        }
+        else
+        {
+            motor.move(1.0f);
+        }
+        if (Vector3.SqrMagnitude(transform.position, waypoints[currentWaypoint].position - transform.position) <= (closeEnough * closeEnough))
+        {
+            if (currentWaypoint < waypoints.Length - 1)
+            {
+                currentWaypoint++;
+            }
+        }
+    }
+}
